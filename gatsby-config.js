73b533c62d0f1ee7910037dae5e2cf75ea7e83e5
Joseph Sungpil Choi <chousemath@gmail.com>
2019-11-10 17:45:09 +0900

    First

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,15 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+      {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Roboto']
+        }
+      }
+    },
+      `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,76 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import styles from '../styles/header.module.scss';
+import ReactSearchBox from 'react-search-box'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const searchData = [
+    {
+      key: 'john',
+      value: 'John Doe',
+    },
+    {
+      key: 'jane',
+      value: 'Jane Doe',
+    },
+    {
+      key: 'mary',
+      value: 'Mary Phillips',
+    },
+    {
+      key: 'robert',
+      value: 'Robert',
+    },
+    {
+      key: 'karius',
+      value: 'Karius',
+    },
+];
+const Header = () => (
+  <header className={styles.mainContainer}>
+    <div className={styles.myLogoContainer}>
+        <div className={styles.myLogoInner}>
+            <img src='/images/logo.png' className={styles.myLogoImage} />
+        </div>
+    </div>
+    <div className={styles.searchContainer}>
+        <ReactSearchBox
+            placeholder="Search here!"
+            value=""
+            data={searchData}
+            callback={record => console.log(record)}
+          />
+        </div>
+    <div className={styles.saleContainer}>Room Ideas</div>
+    <div className={styles.saleContainer}>
+        <span className={styles.saleText}>
+            Sale
+        </span>
+    </div>
+    <div className={styles.accountContainer}>
+        <div className={styles.logoContainer}>
+            <div className={styles.smallIconContainer}>
+                <img src='/images/user.png' className={styles.smallIcon} />
+            </div>
+        </div>
+        <div className={styles.text}>Account</div>
+    </div>
+    <div className={styles.listsContainer}>
+        <div className={styles.logoContainer}>
+            <div className={styles.smallIconContainer}>
+                <img src='/images/list.png' className={styles.smallIcon} />
+            </div>
+        </div>
+        <div className={styles.text}>Lists</div>
+    </div>
+    <div className={styles.cartContainer}>
+        <div className={styles.logoContainer}>
+            <div className={styles.smallIconContainer}>
+                <img src='/images/cart.png' className={styles.smallIcon} />
+            </div>
+        </div>
+        <div className={styles.text}>Cart</div>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
